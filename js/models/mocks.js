86de6065780279a6ce777e4051e7193d953f808d
Daniel Sauble <djsauble@gmail.com>
2016-12-16 08:53:06 -0800

    Add remaining headshots for the IQ team

--- js/models/mocks.js
+++ js/models/mocks.js
@@ -39,6 +39,34 @@ var Mocks = function() {
       }
     },
     {
+      "name": "Andrey Marushkevych",
+      "photo": "images/people/andrey_marushkevych.jpg",
+      "pin": "images/people/map/andrey_marushkevych.png",
+      "role": "Developer",
+      "team": "IQ",
+      "email": "amarushkevych@sonatype.com",
+      "location": {
+        "current": {
+          "lat": 51.253775,
+          "lng": -85.323214
+        }
+      }
+    },
+    {
+      "name": "Benjamin Bentmann",
+      "photo": "images/people/default.png",
+      "pin": "images/people/map/default.png",
+      "role": "Developer",
+      "team": "IQ",
+      "email": "bbentmann@sonatype.com",
+      "location": {
+        "current": {
+          "lat": 51.165691,
+          "lng": 10.451526
+        }
+      }
+    },
+    {
       "name": "Brad Beck",
       "photo": "images/people/brad_beck.jpg",
       "pin": "images/people/map/brad_beck.png",
@@ -49,6 +77,48 @@ var Mocks = function() {
         "current": {
           "lat": 30.267153,
           "lng": -97.743061
+        }
+      }
+    },
+    {
+      "name": "Brandon Murray",
+      "photo": "images/people/default.png",
+      "pin": "images/people/map/default.png",
+      "role": "Developer",
+      "team": "IQ",
+      "email": "bmurray@sonatype.com",
+      "location": {
+        "current": {
+          "lat": 39.320980,
+          "lng": -111.093731
+        }
+      }
+    },
+    {
+      "name": "Brett Casbeer",
+      "photo": "images/people/default.png",
+      "pin": "images/people/map/default.png",
+      "role": "Developer",
+      "team": "IQ",
+      "email": "bcasbeer@sonatype.com",
+      "location": {
+        "current": {
+          "lat": 25.981202,
+          "lng": -80.148379
+        }
+      }
+    },
+    {
+      "name": "Brianne Strozewski",
+      "photo": "images/people/default.png",
+      "pin": "images/people/map/default.png",
+      "role": "Documentation",
+      "team": "IQ",
+      "email": "bstrozewski@sonatype.com",
+      "location": {
+        "current": {
+          "lat": 42.360082,
+          "lng": -71.058880
         }
       }
     },
@@ -77,6 +147,20 @@ var Mocks = function() {
         "current": {
           "lat": 45.523062,
           "lng": -122.676482
+        }
+      }
+    },
+    {
+      "name": "Dennis Warren",
+      "photo": "images/people/default.png",
+      "pin": "images/people/map/default.png",
+      "role": "Developer",
+      "team": "Nexus",
+      "email": "dwarren@sonatype.com",
+      "location": {
+        "current": {
+          "lat": 53.933271,
+          "lng": -116.576504
         }
       }
     },
@@ -123,6 +207,20 @@ var Mocks = function() {
       }
     },
     {
+      "name": "Glen Hunter",
+      "photo": "images/people/glen_hunter.jpg",
+      "pin": "images/people/map/glen_hunter.png",
+      "role": "Developer",
+      "team": "Nexus",
+      "email": "ghunter@sonatype.com",
+      "location": {
+        "current": {
+          "lat": 51.253775,
+          "lng": -85.323214
+        }
+      }
+    },
+    {
       "name": "Kelly Robinson",
       "photo": "images/people/kelly_robinson.jpg",
       "pin": "images/people/map/kelly_robinson.png",
@@ -161,6 +259,48 @@ var Mocks = function() {
         "current": {
           "lat": 40.712784,
           "lng": -74.005941
+        }
+      }
+    },
+    {
+      "name": "Mark Kilby",
+      "photo": "images/people/mark_kilby.jpg",
+      "pin": "images/people/map/mark_kilby.png",
+      "role": "Agile Coach",
+      "team": "IQ",
+      "email": "mkilby@sonatype.com",
+      "location": {
+        "current": {
+          "lat": 28.538335,
+          "lng": -81.379236
+        }
+      }
+    },
+    {
+      "name": "Mark Rekveld",
+      "photo": "images/people/default.png",
+      "pin": "images/people/map/default.png",
+      "role": "Developer",
+      "team": "IQ",
+      "email": "mrekveld@sonatype.com",
+      "location": {
+        "current": {
+          "lat": 52.132633,
+          "lng": 5.291266
+        }
+      }
+    },
+    {
+      "name": "Matthew Piggott",
+      "photo": "images/people/default.png",
+      "pin": "images/people/map/default.png",
+      "role": "Developer",
+      "team": "IQ",
+      "email": "mpiggott@sonatype.com",
+      "location": {
+        "current": {
+          "lat": 51.253775,
+          "lng": -85.323214
         }
       }
     },
@@ -221,6 +361,34 @@ var Mocks = function() {
       }
     },
     {
+      "name": "Ross Pokorny",
+      "photo": "images/people/ross_pokorny.jpg",
+      "pin": "images/people/map/ross_pokorny.png",
+      "role": "Developer",
+      "team": "IQ",
+      "email": "rpokorny@sonatype.com",
+      "location": {
+        "current": {
+          "lat": 39.290385,
+          "lng": -76.612189
+        }
+      }
+    },
+    {
+      "name": "Steven Angles",
+      "photo": "images/people/steven_angles.jpg",
+      "pin": "images/people/map/steven_angles.png",
+      "role": "Developer",
+      "team": "IQ",
+      "email": "sangles@sonatype.com",
+      "location": {
+        "current": {
+          "lat": 39.961176,
+          "lng": -82.998794
+        }
+      }
+    },
+    {
       "name": "Stuart McCulloch",
       "photo": "images/people/stuart_mcculloch.jpg",
       "pin": "images/people/map/stuart_mcculloch.png",
@@ -251,6 +419,48 @@ var Mocks = function() {
         "current": {
           "lat": 38.804836,
           "lng": -77.046921
+        }
+      }
+    },
+    {
+      "name": "Valon Sejdini",
+      "role": "Developer",
+      "photo": "images/people/default.png",
+      "pin": "images/people/map/default.png",
+      "team": "IQ",
+      "email": "vsejdini@sonatype.com",
+      "location": {
+        "current": {
+          "lat": 53.933271,
+          "lng": -116.576504
+        }
+      }
+    },
+    {
+      "name": "Vilius Vaivada",
+      "role": "Developer",
+      "photo": "images/people/vilius_vaivada.jpg",
+      "pin": "images/people/map/vilius_vaivada.png",
+      "team": "IQ",
+      "email": "vvaivada@sonatype.com",
+      "location": {
+        "current": {
+          "lat": 53.800755,
+          "lng": -1.549077
+        }
+      }
+    },
+    {
+      "name": "Vlad Tatavu",
+      "role": "Developer",
+      "photo": "images/people/default.png",
+      "pin": "images/people/map/default.png",
+      "team": "IQ",
+      "email": "vtatavu@sonatype.com",
+      "location": {
+        "current": {
+          "lat": 53.800755,
+          "lng": -1.549077
         }
       }
     },
